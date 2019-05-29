ba65a01ac5d98f8cc684af4570962f0de1210042
oabulu <oabulu@oabulu-ThinkPad-T480>
2019-05-29 14:52:03 -0500

    User Dom Object

--- src/main/java/com/slg/costsplitter/user/User.java
+++ src/main/java/com/slg/costsplitter/user/User.java
@@ -1,5 +1,33 @@
 package com.slg.costsplitter.user;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="user_table")
+public class User {
+	
+	@Id
+	private Long id;
+	
+	@Column(name="name")
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
